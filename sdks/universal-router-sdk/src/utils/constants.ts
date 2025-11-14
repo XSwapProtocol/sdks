f296aa4bb9d587eafc715673d781307e2ee0be71
mrp <pro100skm@yandex.ru>
2025-11-15 02:12:53 +0300

    fix(universal-router-sdk): PERMIT2 as function

--- sdks/universal-router-sdk/src/utils/constants.ts
+++ sdks/universal-router-sdk/src/utils/constants.ts
@@ -2,6 +2,7 @@ import { ChainId, WETH9 } from '@x-swap-protocol/sdk-core'
 import { BigNumber } from 'ethers'
 
 type ChainConfig = {
+  permit2: string
   router: string
   beta: string
   creationBlock: number
@@ -13,12 +14,14 @@ const WETH_NOT_SUPPORTED_ON_CHAIN = '0x0000000000000000000000000000000000000000'
 const CHAIN_CONFIGS: { [key in ChainId]: ChainConfig } = {
   // mainnet
   [ChainId.XDC]: {
+    permit2: '0x941acf4e2df51bf43c3c4167631dbefa268bc9d7',
     router: '0xe1bcb1c502a545ee85a1881b95cdd46d394d2b2e',
     beta: '0x16BC9b9fA8c48c9c6D3F96b53D3C94be125dcA82',
     weth: WETH9[ChainId.XDC].address,
     creationBlock: 59782467,
   },
   [ChainId.APOTHEM]: {
+    permit2: '0x4b722f4a38f97e4078260de0c47f34ae0c404dbf',
     router: '0xef53145eaa955f0b7749a80315de815e383540fb',
     beta: '',
     weth: WETH9[ChainId.APOTHEM].address,
@@ -37,7 +40,7 @@ export const UNIVERSAL_ROUTER_CREATION_BLOCK = (chainId: number): number => {
 }
 
 export const WETH_ADDRESS = (chainId: number): string => {
-  if (!(chainId in CHAIN_CONFIGS)) throw new Error(`Universal Router not deployed on chain ${chainId}`)
+  if (!(chainId in CHAIN_CONFIGS)) throw new Error(`Wrapped Token not deployed on chain ${chainId}`)
 
   if (CHAIN_CONFIGS[chainId as ChainId].weth == WETH_NOT_SUPPORTED_ON_CHAIN)
     throw new Error(`Chain ${chainId} does not have WETH`)
@@ -45,9 +48,10 @@ export const WETH_ADDRESS = (chainId: number): string => {
   return CHAIN_CONFIGS[chainId as ChainId].weth
 }
 
-export const PERMIT2_ADDRESSES: { [chainId in ChainId]: string } = {
-  [ChainId.XDC]: '0x941acf4e2df51bf43c3c4167631dbefa268bc9d7',
-  [ChainId.APOTHEM]: '0x4b722f4a38f97e4078260de0c47f34ae0c404dbf',
+export const PERMIT2_ADDRESSES = (chainId: number): string => {
+  if (!(chainId in CHAIN_CONFIGS)) throw new Error(`Permit not deployed on chain ${chainId}`)
+
+  return CHAIN_CONFIGS[chainId as ChainId].permit2
 }
 
 export const CONTRACT_BALANCE = BigNumber.from(2).pow(255)
